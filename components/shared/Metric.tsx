88406c28a2f9b78e4f2302e643e284b2073556bd
Philip Boen <philipboen001@gmail.com>
2024-01-13 18:56:30 +0300

    modify homepage

---
+++ components/shared/Metric.tsx
@@ -0,0 +1,58 @@
+import Image from "next/image";
+import Link from "next/link";
+
+interface MetricProps {
+  imgUrl: string;
+  alt: string;
+  value: number | string;
+  title: string;
+  href?: string;
+  textStyles?: string;
+  isAuthor?: boolean;
+}
+
+const Metric = ({
+  imgUrl,
+  alt,
+  value,
+  title,
+  href,
+  textStyles,
+  isAuthor,
+}: MetricProps) => {
+  const metricContent = (
+    <>
+      <Image
+        src={imgUrl}
+        alt={alt}
+        width={20}
+        height={20}
+        className={`object-contain ${href ? "rounded-full" : ""}`}
+      />
+
+      <p className={`${textStyles} flex items-center gap-1`}>
+        {value}
+
+        <span
+          className={`xs-regular line-clamp-1 self-end ${
+            isAuthor ? "max-sm:hidden" : ""
+          }`}
+        >
+          {title}
+        </span>
+      </p>
+    </>
+  );
+
+  if (href) {
+    return (
+      <Link href={href} className="flex items-end gap-1">
+        {metricContent}
+      </Link>
+    );
+  }
+
+  return <div className="flex items-end gap-1">{metricContent}</div>;
+};
+
+export default Metric;
--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,62 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getTimestamp = (createdAt: Date): string => {
+  const now = new Date()
+  const timeDifference = now.getTime() - createdAt.getTime()
+
+  const minute = 60 * 1000
+  const hour = minute * 60
+  const day = hour * 24
+  const week = day * 7
+  const month = week * 4
+  const year = month * 12
+
+  if (timeDifference < minute) {
+    return "just now"
+  }
+  else if (timeDifference < hour) {
+    const minutes = Math.floor(timeDifference / minute)
+    return `${minutes} ${minutes > 1 ? "minutes" : "minute"} ago`
+  }
+  else if (timeDifference < day) {
+    const hours = Math.floor(timeDifference / hour)
+    return `${hours} ${hours > 1 ? "hours" : "hour"} ago`
+  }
+  else if (timeDifference < week) {
+    const days = Math.floor(timeDifference / day)
+    return `${days} ${days > 1 ? "days" : "day"} ago`
+  }
+  else if (timeDifference < month) {
+    const weeks = Math.floor(timeDifference / week)
+    return `${weeks} ${weeks > 1 ? "weeks" : "week"} ago`
+  }
+  else if (timeDifference < year) {
+    const months = Math.floor(timeDifference / month)
+    return `${months} ${months > 1 ? "months" : "month"} ago`
+  }
+  else {
+    const years = Math.floor(timeDifference / year)
+    return `${years} ${years > 1 ? "years" : "year"} ago`
+  }
+}
+
+
+export const formatNumber = (num: number): string => {
+  if (num >= 1000000000) {
+    const value = num / 1000000000;
+    return `${value % 1 === 0 ? Math.floor(value) : value.toFixed(1)}B`
+  }
+  else if (num >= 1000000) {
+    const value = num / 1000000;
+    return `${value % 1 === 0 ? Math.floor(value) : value.toFixed(1)}M`
+  }
+  else if (num >= 1000) {
+    const value = num / 1000;
+    return `${value % 1 === 0 ? Math.floor(value) : value.toFixed(1)}K`
+  }
+  else {
+    return num.toString()
+  }
+}
