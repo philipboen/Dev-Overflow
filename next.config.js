cb57dc72e819e75217929730dcac89b6c1f98087
Philip Boen <philipboen001@gmail.com>
2024-08-19 12:44:44 +0300

    refactor: update CSP with style-src and 'unsafe-inline'

--- next.config.js
+++ next.config.js
@@ -20,7 +20,7 @@ const nextConfig = {
           {
             key: "Content-Security-Policy",
             value:
-              "default-src 'self'; script-src 'self' style-src 'self' 'unsafe-inline",
+              "default-src 'self'; script-src 'self' style-src 'self' 'unsafe-inline'",
           },
           {
             key: "Strict-Transport-Security",
