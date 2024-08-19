f1ad986727c9dbd834b046a869c0096d1022ee26
Philip Boen <philipboen001@gmail.com>
2024-08-19 12:08:42 +0300

    chore: Add security headers to next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,52 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply these headers to all routes
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: "default-src 'self'; script-src 'self'",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "no-referrer",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              "accelerometer=(), autoplay=(), camera=(), cross-origin-isolated=(), display-capture=(), encrypted-media=(), fullscreen=(self), geolocation=(), gyroscope=(), keyboard-map=(), magnetometer=(), microphone=(), midi=(), payment=(), picture-in-picture=(), publickey-credentials-get=(), screen-wake-lock=(), sync-xhr=(self), usb=(), web-share=(), xr-spatial-tracking=(), clipboard-read=(), clipboard-write=(), gamepad=(), hid=(), idle-detection=(), interest-cohort=(), serial=(), unload=()",
+          },
+          {
+            key: "Expect-CT",
+            value: 'max-age=86400, enforce"',
+          },
+          {
+            key: "Clear-Site-Data",
+            value: '"cache", "cookies", "storage", "executionContexts"',
+          },
+        ],
+      },
+    ];
+  },
 };
 
-module.exports = nextConfig
+module.exports = nextConfig;
